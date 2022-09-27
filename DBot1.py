8044824397c6244b9dbff3ab3870b113d79c5fb4
Lia Pires <liasth.pires@gmail.com>
2022-09-27 14:29:04 -0300

    Update Dbot1.py

--- DBot1.py
+++ DBot1.py
@@ -1,24 +1,43 @@
+
 import discord
+import discord.ext
 from discord.ext import commands, tasks
 import datetime
 
 bot = commands.Bot("!")
 
 
+
 @bot.event
 async def on_ready():
     channel = bot.get_channel(1023995162973708301)
+    print(f"Estou pronto {bot.user}")
     await channel.send('Olá, seja bem vindo!')
     await channel.send('Eu sou o DBot1.')
-    current_time.start()
 
 
-@tasks.loop(hours=1)
-async def current_time():
+@bot.command(name="oi")
+async def send_hello(ctx):
+    name = ctx.author.name
+    response = "Olá, " + name + "!"
+    await ctx.send(response)  
+
+@bot.command(name="data")
+async def send_date(ctx):
     now = datetime.datetime.now()
-    now = now.strftime("%d/%m/%Y às %H:%M:%S")
-    channel = bot.get_channel(1023995162973708301)
-    await channel.send("Data e hora: " + now)
+    now = now.strftime("%d/%m/%Y")
+    response = "Hoje é " + now
+    await ctx.send(response)
+
+@bot.command(name="hora")
+async def send_date(ctx):
+    now = datetime.datetime.now()
+    now = now.strftime("%H:%M:%S")
+    response = "Horário atual: " + now
+    await ctx.send(response)
+
+
+
 
 # @bot.command(name="data")
 # async def current_time():
@@ -27,4 +46,6 @@ async def current_time():
 #     channel = bot.get_channel(1023995162973708301)
 #     await channel.send("Data e hora: " + now)
 
-bot.run('MTAyNDA3NjY5NzI4MjI4MTU0Mg.GITZe8.i_5GAvFe4Uof3Mxr-yw-CHPO4wSeZFDDZHuvwQ')
+bot.run('MTAyNDA3NjY5NzI4MjI4MTU0Mg.Gba5Yq.UghIsIEGbUonxxEywbCDnWTGwoX3VrMh69KgJ8')
+
+
