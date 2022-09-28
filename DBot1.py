12dfd4b9be19baa6205d7bf03d6d1c7cfdb6a02e
Lia Pires <liasth.pires@gmail.com>
2022-09-28 14:13:17 -0300

    Update DBot1.py
    Adding advice command

--- DBot1.py
+++ DBot1.py
@@ -6,15 +6,22 @@ import datetime
 import requests
 import numpy as np
 
+'''
+Versões instaladas:
+discord-ext-bot==1.0.1
+discord.py==1.7.3
+numpy==1.23.3
+requests==2.27.1
+Python 3.10.4
+'''
+
 bot = commands.Bot("!")
 
 @bot.event
 async def on_ready():
     channel = bot.get_channel(1023995162973708301)
     print(f"Estou pronto {bot.user}")
-    await channel.send('Olá, seja bem vindo!')
-    await channel.send('Eu sou o DBot1.')
-
+    await channel.send('Olá, eu sou o DBot1.')
 
 @bot.command(name="oi")
 async def send_hello(ctx):
@@ -47,10 +54,8 @@ async def send_db1(ctx):
     response = "O Grupo DB1 é formado por empresas de tecnologia, com sede em Maringá – PR – com 3 operações no Brasil, uma na Argentina e uma nos Estados Unidos. Tem 20 anos de experiência no mercado de tecnologia da informação e oferece software e serviços de desenvolvimento para empresas (B2B). A DB1 Global Software representa 50% do grupo, o prestigiando com clientes nacionais e internacionais. O Grupo DB1 nasceu DB1 Global Software, sendo essa a primeira marca e o core do negócio. Ainda hoje, esse legado de ter tecnologia no DNA se reflete em cada nova empresa do grupo. Sempre com posicionamento de produtos e serviços “premium” e de alta qualidade as empresas do grupo são líderes de mercado como ANYMARKET, Consignet, DB1 Global Software, Koncili, EIVE, Predize e o robô Tinbot." 
     await ctx.send(response)
 
-
 @bot.command("cotacao")
 async def binance(ctx):
-
     response = requests.get(
         f"https://api.binance.com/api/v3/ticker/price?symbol=USDTBRL"
     )
@@ -58,6 +63,32 @@ async def binance(ctx):
     pricestr = data.get("price")
     price = float(pricestr)
     await ctx.send(f"No momento US$ 1.00 vale R$ {price:.2f}")
+
+
+def get_nested(data, *args):
+    if args and data:
+        element  = args[0]
+        if element:
+            value = data.get(element)
+            return value if len(args) == 1 else get_nested(value, *args[1:])
+
+
+@bot.command("advice")
+async def advice(ctx):
+    response = requests.get(f"https://api.adviceslip.com/advice")
+    data = response.json()
+    advice = get_nested(data, "slip", "advice")
+    await ctx.send(f"{advice}")
+
+
+# @tasks.loop(seconds=3)
+# async def advice(ctx):
+#     response = requests.get(
+#         f"https://api.adviceslip.com/advice"
+#     )
+#     data = response.json()
+#     advice = data.get("advice")
+#     await ctx.send(f"Sorte de hoje: {advice}")
 
 
 
